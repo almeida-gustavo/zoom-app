e316d01c12d4bd4090fa317d4a33e2b20bc499be
melogustavo <gustavomelo95@gmail.com>
2021-01-27 03:06:16 -0300

    aula 2

--- public/pages/room/src/app.js
+++ public/pages/room/src/app.js
@@ -17,6 +17,17 @@ const onload = () => {
   // eh a url do servidor que ta rodando
   const socketUrl = 'http://localhost:3000';
   const socketBuilder = new SocketBuilder({ socketUrl });
+
+  const peerConfig = Object.values({
+    id: undefined,
+    config: {
+      port: 9000,
+      host: 'localhost',
+      path: '/',
+    },
+  });
+  const peerBuilder = new PeerBuilder({ peerConfig });
+
   const view = new View();
   const media = new Media();
   const deps = {
@@ -24,6 +35,7 @@ const onload = () => {
     media,
     room,
     socketBuilder,
+    peerBuilder,
   };
 
   Business.initialize(deps);
